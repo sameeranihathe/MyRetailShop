7a5ebf0116179c887bda16b3b6a4c2301b9449d0
Sameera Nihathamana <sameeranihathe@gmail.com>
2021-06-26 08:46:20 +0530

    Add desktop UI

---
+++ RetailWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,52 @@
+﻿using Caliburn.Micro;
+using RetailWPFUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace RetailWPFUserInterface
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _simpleContainer = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _simpleContainer.Instance(_simpleContainer);
+            _simpleContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _simpleContainer.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _simpleContainer.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _simpleContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _simpleContainer.BuildUp(instance);
+        }
+    }
+}
---
+++ RetailWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailWPFUserInterface.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}

38b9457deb32c723c50c1b6449e7713ed394ac5b
Sameera Nihathamana <sameeranihathe@gmail.com>
2021-07-18 09:43:40 +0530

    add database tables

---
+++ RetailData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] NCHAR(10) NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
